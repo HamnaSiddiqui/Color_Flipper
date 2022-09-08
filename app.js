9234d5b89c2a2b4c4b51d320bc0e64201ad0b187
Hamna Siddiqui <hamnas200@gmail.com>
2022-09-08 11:13:49 -0700

    making

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-let colors = ["red", "green","blue","rgba"]
+const colors = ["green","red","rgba(133, 122. 200)","#f15025"]
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+    // console.log(document.body);
+    // get random Number between 0 and 3 colors[0]
+    const randomNumber = 2;
+    document.body.style.backgroundColor = color[randomNumber];
+    color.textContent = color[randomNumber];
+})
