5675f7dde6bdb574458b36a39c0d045931ca232d
Hamna Siddiqui <hamnas200@gmail.com>
2022-09-08 11:23:45 -0700

    made it bingo

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const color = document.querySelector(".color");
 btn.addEventListener('click', function(){
     // console.log(document.body);
     // get random Number between 0 and 3 colors[0]
-    const randomNumber = 2;
-    document.body.style.backgroundColor = color[randomNumber];
-    color.textContent = color[randomNumber];
+    const randomNumber = getrandomNumber();
+    // console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 })
+
+function getrandomNumber(){
+    return Math.floor(Math.random() *colors.length);
+}
